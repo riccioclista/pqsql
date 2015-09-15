bc7c23340b2152b7e2edd77c7c4fd4a9581611ef
Thomas Krennwallner <krennwallner@ximes.com>
2015-09-15 16:26:06 +0000

    add PqsqlBinaryFormat.cs for encoding / decoding .NET datatypes from / to raw binary messages add winsock.cs for select() and friends (used in non-blocking connection start mode)
    git-tfs-id: [http://xiserver38:8080/tfs/XIMES]$/TIS/TRUNK;C41599

---
+++ PqsqlBinaryFormat.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace Pqsql
+{
+	[StructLayout(LayoutKind.Explicit)]
+	internal struct swap_float4_int4_union
+	{
+		[FieldOffset(0)]
+		public uint as_unsigned;
+
+		[FieldOffset(0)]
+		public float as_single_precision;
+	}
+
+
+	[StructLayout(LayoutKind.Explicit)]
+	internal struct swap_float8_int8_union
+	{
+		[FieldOffset(0)]
+		public ulong as_unsigned;
+
+		[FieldOffset(0)]
+		public double as_double_precision;
+	}
+
+
+	//
+	// Routines for formatting and parsing frontend/backend binary messages
+	//
+	public unsafe class PqsqlBinaryFormat
+	{
+		#region encode datatype to binary message
+
+		// see pq_sendtext(StringInfo buf, const char *str, int slen) from pqformat.c
+		public static void SendText(ref IntPtr val, string s)
+		{
+			Marshal.StructureToPtr(s, val, true);
+		}
+
+
+		// see pq_sendint(StringInfo buf, int i, int b) from pqformat.c
+		public static void SendInt(ref IntPtr val, int i, int b)
+		{
+       switch (b)
+       {
+				 case 1:
+					 val = (IntPtr)(byte)i;
+					 break;
+				 case 2:
+					 val = (IntPtr)(ushort)IPAddress.HostToNetworkOrder((ushort)i);
+					 break;
+				 case 4:
+					 val = (IntPtr)(uint)IPAddress.HostToNetworkOrder((uint)i);
+					 break;
+				 default:
+					 throw new InvalidCastException("unsupported integer size " + b.ToString());
+			 }
+		}
+
+
+		// see pq_sendint64(StringInfo buf, int64 i) from pqformat.c
+		public static void SendInt64(ref IntPtr val, long i)
+		{
+			uint h32;
+			uint l32;
+
+			/* High order half first, since we're doing MSB-first */
+			h32 = (uint)IPAddress.HostToNetworkOrder((uint)(i >> 32));
+
+			/* Now the low order half */
+			l32 = (uint)IPAddress.HostToNetworkOrder((uint)i);
+
+			i = l32;
+			i <<= 32;
+			i |= h32;
+
+			val = (IntPtr)i;
+		}
+
+
+		// see void pq_sendfloat4(StringInfo msg, float4 f) from pqformat.c
+		public static void SendFloat4(ref IntPtr val, float f)
+		{
+			swap_float4_int4_union onion = new swap_float4_int4_union();
+			onion.as_single_precision = f;
+			val = (IntPtr)IPAddress.HostToNetworkOrder(onion.as_unsigned);
+		}
+
+
+		// see void pq_sendfloat8(StringInfo msg, float8 f) from pqformat.c
+		public static void SendFloat8(ref IntPtr val, double d)
+		{
+			swap_float8_int8_union onion = new swap_float8_int8_union();
+			onion.as_double_precision = d;
+			SendInt64(ref val, (long)onion.as_unsigned);
+		}
+
+		#endregion
+
+
+		#region decode datatype from binary message
+
+		// see char* pq_getmsgstring(StringInfo msg) from pqformat.c
+		public static string GetMsgString(IntPtr val)
+		{
+			return Marshal.PtrToStringAnsi(val);
+		}
+
+
+		// see int pq_getmsgint(StringInfo msg) from pqformat.c 	
+		public static int GetMsgInt(IntPtr val, int b)
+		{
+			uint res;
+			void* v = (void*)val;
+
+			switch (b)
+			{
+				case 1:
+					res = *(byte*)v;
+					break;
+				case 2:
+					res = (uint)IPAddress.NetworkToHostOrder(*(ushort*)v);
+					break;
+				case 4:
+					res = (uint)IPAddress.NetworkToHostOrder(*(uint*)v);
+					break;
+				default:
+					throw new InvalidCastException("unsupported integer size " + b.ToString());
+			}
+
+			return (int)res;
+		}
+
+
+		// see int pq_getmsgint64(StringInfo msg) from pqformat.c 	
+		public static long GetMsgInt64(IntPtr val)
+		{
+			long res;
+			uint h32;
+			uint l32;
+			void* hv = (void*)val;
+			void* lv = (void*)(val + 4);
+
+			h32 = (uint)IPAddress.NetworkToHostOrder(*(uint*)hv);
+			l32 = (uint)IPAddress.NetworkToHostOrder(*(uint*)lv);
+
+			res = h32;
+			res <<= 32;
+			res |= l32;
+
+			return res;
+		}
+
+
+		// see float4 pq_getmsgfloat4(StringInfo msg) from pqformat.c 	
+		public static float GetMsgFloat4(IntPtr val)
+		{
+			swap_float4_int4_union onion = new swap_float4_int4_union();
+			onion.as_unsigned = (uint)IPAddress.NetworkToHostOrder(*((int*)((void*)val)));
+			return onion.as_single_precision;
+		}
+
+
+		// see float8 pq_getmsgfloat8(StringInfo msg) from pqformat.c 	
+		public static double GetMsgFloat8(IntPtr val)
+		{
+			swap_float8_int8_union onion = new swap_float8_int8_union();
+			onion.as_unsigned = (ulong)IPAddress.NetworkToHostOrder(*((long*)((void*)val)));
+			return onion.as_double_precision;
+		}
+
+		#endregion
+	}
+}
---
+++ winsock.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using SocketError = System.Net.Sockets.SocketError; // reuse SocketError
+
+namespace WinSock2
+{
+
+	// Enums from Microsofts website (#defined in C++)
+	public enum AddressFamily : int
+	{
+		Unknown = 0,
+		InterNetworkv4 = 2,
+		Ipx = 4,
+		AppleTalk = 17,
+		NetBios = 17,
+		InterNetworkv6 = 23,
+		Irda = 26,
+		BlueTooth = 32
+	}
+
+	public enum SocketType : int
+	{
+		Unknown = 0,
+		Stream = 1,
+		DGram = 2,
+		Raw = 3,
+		Rdm = 4,
+		SeqPacket = 5
+	}
+
+	public enum ProtocolType : int
+	{
+		BlueTooth = 3,
+		Tcp = 6,
+		Udp = 17,
+		ReliableMulticast = 113
+	}
+
+	// Equivilent to C++s "SOCKET"
+	public unsafe struct SOCKET
+	{
+		private void* handle;
+		private SOCKET(int _handle)
+		{
+			handle = (void*)_handle;
+		}
+		public static bool operator ==(SOCKET s, int i)
+		{
+			return ((int)s.handle == i);
+		}
+		public static bool operator !=(SOCKET s, int i)
+		{
+			return ((int)s.handle != i);
+		}
+		public static implicit operator SOCKET(int i)
+		{
+			return new SOCKET(i);
+		}
+		public static implicit operator uint(SOCKET s)
+		{
+			return (uint)s.handle;
+		}
+		public override bool Equals(object obj)
+		{
+			return (obj is SOCKET) ? (((SOCKET)obj).handle == this.handle) : base.Equals(obj);
+		}
+		public override int GetHashCode()
+		{
+			return (int)handle;
+		}
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct fd_set
+	{
+		public uint fd_count;
+
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = Size)]
+		public IntPtr[] fd_array;
+
+		private const int Size = 1;
+
+		public static fd_set Null
+		{
+			get
+			{
+				return new fd_set()
+				{
+					fd_array = null,
+					fd_count = 0
+				};
+			}
+		}
+
+		public static fd_set Create(IntPtr socket)
+		{
+			var handle = new fd_set()
+			{
+				fd_count = Size,
+				fd_array = new IntPtr[Size] { socket }
+			};
+			return handle;
+		}
+	}
+
+	// C# equivilent to C++'s sockaddr_in / SOCKADDR_IN
+	[StructLayout(LayoutKind.Sequential, Size = 16)]
+	public struct sockaddr_in
+	{
+		public const int Size = 16;
+
+		public short sin_family;
+		public ushort sin_port;
+		public struct in_addr
+		{
+			public uint S_addr;
+			public struct _S_un_b
+			{
+				public byte s_b1, s_b2, s_b3, s_b4;
+			}
+			public _S_un_b S_un_b;
+			public struct _S_un_w
+			{
+				public ushort s_w1, s_w2;
+			}
+			public _S_un_w S_un_w;
+		}
+		public in_addr sin_addr;
+	}
+
+	// WSAData structure, used in WSAStarutp
+	[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+	public unsafe struct WSAData
+	{
+		public ushort Version;
+		public ushort HighVersion;
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 257)]
+		public string Description;
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 129)]
+		public string SystemStatus;
+		public ushort MaxSockets;
+		public ushort MaxUdpDg;
+		sbyte* lpVendorInfo;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct timeval
+	{
+		/// <summary>
+		/// Time interval, in seconds.
+		/// </summary>
+		public int tv_sec;
+
+		/// <summary>
+		/// Time interval, in microseconds.
+		/// </summary>
+		public int tv_usec;
+	};
+
+	// Interface to ws2_32.dll
+	public unsafe partial class winsock
+	{
+		public const int SOCKET_ERROR = -1;
+		public const int INVALID_SOCKET = ~0;
+
+		[DllImport("Ws2_32.dll")]
+		public static extern int WSAStartup(ushort Version, out WSAData Data);
+		[DllImport("Ws2_32.dll")]
+		public static extern SocketError WSAGetLastError();
+		[DllImport("Ws2_32.dll")]
+		public static extern SOCKET socket(AddressFamily af, SocketType type, ProtocolType protocol);
+		[DllImport("Ws2_32.dll")]
+		public static extern int send(SOCKET s, byte* buf, int len, int flags);
+		[DllImport("Ws2_32.dll")]
+		public static extern int recv(SOCKET s, byte* buf, int len, int flags);
+		[DllImport("Ws2_32.dll")]
+		public static extern SOCKET accept(SOCKET s, void* addr, int addrsize);
+		[DllImport("Ws2_32.dll")]
+		public static extern int listen(SOCKET s, int backlog);
+		[DllImport("Ws2_32.dll", CharSet = CharSet.Ansi)]
+		public static extern uint inet_addr(string cp);
+		[DllImport("Ws2_32.dll")]
+		public static extern ushort htons(ushort hostshort);
+		[DllImport("Ws2_32.dll")]
+		public static extern int connect(SOCKET s, sockaddr_in* addr, int addrsize);
+		[DllImport("Ws2_32.dll")]
+		public static extern int closesocket(SOCKET s);
+		[DllImport("Ws2_32.dll")]
+		public static extern int getpeername(SOCKET s, sockaddr_in* addr, int* addrsize);
+		[DllImport("Ws2_32.dll")]
+		public static extern int bind(SOCKET s, sockaddr_in* addr, int addrsize);
+		[DllImport("Ws2_32.dll")]
+		public static extern int select(int ndfs, ref fd_set readfds, ref fd_set writefds, ref fd_set exceptfds, timeval* timeout);
+		[DllImport("Ws2_32.dll")]
+		public static extern sbyte* inet_ntoa(sockaddr_in.in_addr _in);
+	}
+
+
+}
