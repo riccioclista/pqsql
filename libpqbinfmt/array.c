b77df91b7cf7ef4bca9313df1eba13e0ea669df0
Thomas Krennwallner <krennwallner@ximes.com>
2015-11-12 17:37:40 +0100

    libpqbinfmt: add support for array parameters

--- libpqbinfmt/array.c
+++ libpqbinfmt/array.c
@@ -1,8 +1,9 @@
-
 #include <stdio.h>
+#include <stdlib.h>
 
 #define DLL_EXPORT
 #include "pqbinfmt.h"
+#include "pq_types.h"
 
 
 DECLSPEC const char * __fastcall
@@ -17,26 +18,26 @@ pqbf_get_array(const char* p,
 
 	v = (char*) p;
 
-	/* array header */
+	/* 12 byte array header */
 
-	*ndim = pqbf_get_int4(v);
+	*ndim = BYTESWAP4(*((uint32_t *)v));
 	v += sizeof(*ndim);
 
-	*flags = pqbf_get_int4(v);
+	*flags = BYTESWAP4(*((uint32_t *)v));
 	v += sizeof(*flags);
 
-	*o = pqbf_get_oid(v);
+	*o = BYTESWAP4(*((uint32_t *)v));
 	v += sizeof(*o);
 
-	/* dimension header */
+	/* ndim * 8 byte dimension header */
 
 	for (i = 0; i < *ndim; i++)
 	{
 		/* we trust pgsql here for correct lower and upper bounds */
-		*dim[i] = pqbf_get_int4(v);
+		*dim[i] = BYTESWAP4(*((uint32_t *)v));
 		v += sizeof(int);
 
-		*lbound[i] = pqbf_get_int4(v);
+		*lbound[i] = BYTESWAP4(*((uint32_t *)v));
 		v += sizeof(int);
 	}
 
@@ -49,12 +50,81 @@ pqbf_get_array(const char* p,
 DECLSPEC const char * __fastcall
 pqbf_get_array_value(const char* p, int32_t* itemlen)
 {
-	if (p == NULL)
+	if (p == NULL) /* null values are encoded with itemlen=-1 */
 	{
 		*itemlen = -1;
 		return NULL;
 	}
-
-	*itemlen = pqbf_get_int4(p);
+	/* itemlen == -1: null value, otw. we get the number of bytes for the next array item */
+	*itemlen = BYTESWAP4(*((uint32_t *)p));
 	return p + sizeof(int);
+}
+
+
+#define pqbf_encode_array(s,ndim,flags,oid,dim,lbound) \
+	do { \
+		int i; \
+		uint32_t v; \
+		/* 12 byte array header */ \
+		v = BYTESWAP4(ndim); \
+		appendBinaryPQExpBuffer(s, (const char*) &v, sizeof(v)); \
+		v = BYTESWAP4(flags); \
+		appendBinaryPQExpBuffer(s, (const char*) &v, sizeof(v)); \
+		v = BYTESWAP4(oid); \
+		appendBinaryPQExpBuffer(s, (const char*) &v, sizeof(v)); \
+		/* ndim * 8 byte dimension header */ \
+		for (i = 0; i < ndim; i++) { /* we trust pgsql here for correct lower and upper bounds */ \
+			v = BYTESWAP4(dim[i]); \
+			appendBinaryPQExpBuffer(s, (const char*) &v, sizeof(v)); \
+			v = BYTESWAP4(lbound[i]); \
+			appendBinaryPQExpBuffer(s, (const char*) &v, sizeof(v)); \
+		} \
+	} while(0)
+
+
+DECLSPEC void __fastcall
+pqbf_set_array(PQExpBuffer s,
+								int32_t ndim, int32_t flags, uint32_t oid,
+								int dim[MAXDIM],	int lbound[MAXDIM])
+{
+	BAILIFNULL(s);
+	pqbf_encode_array(s, ndim, flags, oid, dim, lbound);
+	/* next comes array data */
+}
+
+
+
+DECLSPEC void __fastcall
+pqbf_set_array_itemlength(PQExpBuffer a, int32_t itemlen)
+{
+	BAILIFNULL(a);
+	itemlen = BYTESWAP4(itemlen);
+	appendBinaryPQExpBuffer(a, (const char*) &itemlen, sizeof(itemlen)); /* add item length */
+	/* next comes array item */
+}
+
+#if 0
+DECLSPEC void __fastcall
+pqbf_set_array_value(PQExpBuffer a, const char* p, int32_t itemlen)
+{
+	BAILIFNULL(a);
+	/* null values have itemlen == -1 */
+	if (p != NULL && itemlen >= 0) /* add non-null value array item */
+		appendBinaryPQExpBuffer(a, p, itemlen);
+}
+#endif
+
+DECLSPEC void __fastcall
+pqbf_add_array(pqparam_buffer *pb, PQExpBuffer a, uint32_t oid)
+{
+	size_t len;
+
+	BAILIFNULL(pb);
+	BAILIFNULL(a);
+
+	len = pb->payload->len; /* save current length of payload */
+
+	appendBinaryPQExpBuffer(pb->payload, a->data, a->len); /* add array header + data */
+
+	pqpb_add(pb, oid,  pb->payload->len - len);
 }
