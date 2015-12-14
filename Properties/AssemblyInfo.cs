0f0c7a9889d6315a9fb017b3958910f0df1e881c
Thomas Krennwallner <krennwallner@ximes.com>
2015-12-14 12:28:03 +0100

    Pqsql: use pgsql versioning scheme

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -7,12 +6,11 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly: AssemblyTitle("Pqsql")]
-[assembly: AssemblyDescription(".NET data provider for PostgreSQL using libpq")]
+[assembly: AssemblyDescription("PostgreSQL .NET data provider using libpq")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("XIMES GmbH")]
 [assembly: AssemblyProduct("Pqsql")]
 [assembly: AssemblyCopyright("Copyright © XIMES GmbH 2015")]
-[assembly: AssemblyTrademark("Time Intelligence ®")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -33,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("9.4.5.0")]
+[assembly: AssemblyFileVersion("9.4.5.0")]
