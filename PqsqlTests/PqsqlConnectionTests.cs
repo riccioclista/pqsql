ae38f8a2107d2d2b0807972f3d53ae566ffdfa2a
Thomas Krennwallner <krennwallner@ximes.com>
2016-10-20 18:48:42 +0200

    PqsqlTests: add unit tests

---
+++ PqsqlTests/PqsqlConnectionTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pqsql;
+
+namespace PqsqlTests
+{
+	/// <summary>
+	/// Summary description for PqsqlConnectionTests
+	/// </summary>
+	[TestClass]
+	public class PqsqlConnectionTests
+	{
+
+		private TestContext testContextInstance;
+
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext
+		{
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+		}
+
+		#region Additional test attributes
+		//
+		// You can use the following additional attributes as you write your tests:
+		//
+		// Use ClassInitialize to run code before running the first test in the class
+		// [ClassInitialize()]
+		// public static void MyClassInitialize(TestContext testContext) { }
+		//
+		// Use ClassCleanup to run code after all tests in a class have run
+		// [ClassCleanup()]
+		// public static void MyClassCleanup() { }
+		//
+		// Use TestInitialize to run code before running each test 
+		// [TestInitialize()]
+		// public void MyTestInitialize() { }
+		//
+		// Use TestCleanup to run code after each test has run
+		// [TestCleanup()]
+		// public void MyTestCleanup() { }
+		//
+		#endregion
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+			PqsqlConnection connection = new PqsqlConnection("");
+
+			Assert.AreEqual(ConnectionState.Closed, connection.State, "wrong connection state");
+			Assert.AreEqual(String.Empty, connection.ConnectionString, "wrong connection string");
+			Assert.AreEqual(0, connection.ConnectionTimeout, "wrong connection timeout");
+			Assert.AreEqual(String.Empty, connection.Database, "wrong connection database");
+		}
+
+		[TestMethod]
+		public void TestMethod2()
+		{
+			PqsqlConnection connection = new PqsqlConnection("host=localhost; port=5432; dbname=postgres; connect_timeout=3");
+
+			Assert.AreEqual(ConnectionState.Closed, connection.State, "wrong connection state");
+			Assert.AreEqual("host=localhost;port=5432;dbname=postgres;connect_timeout=3", connection.ConnectionString, "wrong connection string");
+			Assert.AreEqual(3, connection.ConnectionTimeout, "wrong connection timeout");
+			Assert.AreEqual("postgres", connection.Database, "wrong connection database");
+
+			connection.Open();
+
+			Assert.AreEqual(ConnectionState.Open, connection.State, "wrong connection state");
+
+			connection.Close();
+
+			Assert.AreEqual(ConnectionState.Closed, connection.State, "wrong connection state");
+		}
+	}
+}
---
+++ PqsqlTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PqsqlTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("XIMES GmbH")]
+[assembly: AssemblyProduct("PqsqlTests")]
+[assembly: AssemblyCopyright("Copyright © XIMES GmbH 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("521c5b99-76e7-425c-84ca-853d021e8e5b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("9.4.9.0")]
+[assembly: AssemblyFileVersion("9.4.9.0")]
