d4da1993bf257e26ca69440cafc011e51939807b
Thomas Krennwallner <krennwallner@ximes.com>
2016-11-02 10:00:25 +0100

    Pqsql: bump version to 9.4.10.0
    FLEX-1222

--- PqsqlTests/Properties/AssemblyInfo.cs
+++ PqsqlTests/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("9.4.9.0")]
-[assembly: AssemblyFileVersion("9.4.9.0")]
+[assembly: AssemblyVersion("9.4.10.0")]
+[assembly: AssemblyFileVersion("9.4.10.0")]
