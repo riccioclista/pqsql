189068c0ea8bbb99848a6f8952bc2140961e8a5b
Thomas Krennwallner <krennwallner@ximes.com>
2017-03-30 11:51:13 +0200

    PqsqlDataAdapterTest2: new test

--- PqsqlTests/PqsqlDataAdapterTests.cs
+++ PqsqlTests/PqsqlDataAdapterTests.cs
@@ -101,5 +101,63 @@ namespace PqsqlTests
 			Assert.AreEqual(1, rows, "wrong row count");
 			Assert.AreEqual(5, columns, "wrong column count");
 		}
+
+		[TestMethod]
+		public void PqsqlDataAdapterTest2()
+		{
+			// get current_database() name
+			mCmd.CommandText = "current_database";
+			mCmd.CommandType = CommandType.StoredProcedure;
+			object curdb = mCmd.ExecuteScalar();
+
+			// fetch pg_database tuple
+			string select = "select datname,pg_encoding_to_char(encoding),datcollate,datctype,datallowconn from pg_database where datname='" + curdb + "'";
+
+			DataSet ds = new DataSet();
+			using (PqsqlDataAdapter adapter = new PqsqlDataAdapter(select, connectionString))
+			{
+				adapter.Fill(ds);
+			}
+
+			Assert.AreEqual(1, ds.Tables.Count, "wrong table count");
+
+			int tables = 0;
+			int rows = 0;
+			int columns = 0;
+
+			foreach (DataTable table in ds.Tables)
+			{
+				foreach (DataRow row in table.Rows)
+				{
+					foreach (object item in row.ItemArray)
+					{
+						// read item
+						switch (columns)
+						{
+							case 0: // datname
+								Assert.AreEqual("postgres", item, "wrong database name");
+								break;
+							case 1: // encoding
+								Assert.AreEqual("UTF8", item, "wrong encoding id");
+								break;
+							case 2: // datcollate
+							case 3: // datctype
+								Assert.IsNotNull(item);
+								break;
+							case 4: // datallowconn
+								Assert.AreEqual(true, item, "we must be allowed to connect");
+								break;
+						}
+						columns++;
+					}
+					rows++;
+				}
+				tables++;
+			}
+
+			Assert.AreEqual(1, tables, "wrong table count");
+			Assert.AreEqual(1, rows, "wrong row count");
+			Assert.AreEqual(5, columns, "wrong column count");
+		}
 	}
 }
