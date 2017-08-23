2ce3e6dc995623ae47d6a1d12e9b48f6717bf99d
Thomas Krennwallner <krennwallner@ximes.com>
2017-08-23 17:15:41 +0200

    numeric.h: update src/include/utils/numeric.h from PostgreSQL 9.6.4
    FLEX-2077

--- libpqbinfmt/numeric.h
+++ libpqbinfmt/numeric.h
@@ -1,3 +1,10 @@
+/**
+ * @file numeric.h
+ * @brief encode/decode numeric binary format to native datatype for PQgetvalue() and pqparam_buffer
+ * @date 2015-09-30
+ * @author Thomas Krennwallner <krennwallner@ximes.com>
+ * @see https://www.postgresql.org/docs/current/static/libpq-exec.html
+ */
 /*-------------------------------------------------------------------------
  *
  * numeric.h
@@ -5,26 +12,22 @@
  *
  * Original coding 1998, Jan Wieck.  Heavily revised 2003, Tom Lane.
  *
- * Copyright (c) 1998-2015, PostgreSQL Global Development Group
+ * Copyright (c) 1998-2016, PostgreSQL Global Development Group
  *
  * src/include/utils/numeric.h
  *
  *-------------------------------------------------------------------------
  */
-/**
- * @file numeric.h
- * @brief encode/decode numeric binary format to native datatype for PQgetvalue() and pqparam_buffer
- * @date 2015-09-30 
- * @author Thomas Krennwallner <krennwallner@ximes.com>
- * @see https://www.postgresql.org/docs/current/static/libpq-exec.html
- */
-
 #ifndef _PG_NUMERIC_H_
 #define _PG_NUMERIC_H_
 
+#include "c.h"
+#include "postgres.h"
+
 /*
- * Hardcoded precision limit - arbitrary, but must be small enough that
- * dscale values will fit in 14 bits.
+ * Limit on the precision (and hence scale) specifiable in a NUMERIC typmod.
+ * Note that the implementation limit on the length of a numeric value is
+ * much larger --- beware of what you use this for!
  */
 #define NUMERIC_MAX_PRECISION		1000
 
@@ -46,5 +49,16 @@
 /* The actual contents of Numeric are private to numeric.c */
 struct NumericData;
 typedef struct NumericData *Numeric;
+
+/*
+ * fmgr interface macros
+ */
+
+#define DatumGetNumeric(X)		  ((Numeric) DatumGetPointer(X))
+#define DatumGetNumericCopy(X)	  ((Numeric) PG_DETOAST_DATUM_COPY(X))
+#define NumericGetDatum(X)		  PointerGetDatum(X)
+#define PG_GETARG_NUMERIC(n)	  DatumGetNumeric(PG_GETARG_DATUM(n))
+#define PG_GETARG_NUMERIC_COPY(n) DatumGetNumericCopy(PG_GETARG_DATUM(n))
+#define PG_RETURN_NUMERIC(x)	  return NumericGetDatum(x)
 
 #endif   /* _PG_NUMERIC_H_ */
