f489ead470b15b06d38046fc4c3ec4866d17ff36
Thomas Krennwallner <krennwallner@ximes.com>
2017-08-31 18:25:06 +0200

    Pqsql: bump version to 9.6.5.0
    FLEX-2077

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("9.6.4.2")]
-[assembly: AssemblyFileVersion("9.6.4.2")]
+[assembly: AssemblyVersion("9.6.5.0")]
+[assembly: AssemblyFileVersion("9.6.5.0")]
 
 #if (DEBUG || TEST)
 
