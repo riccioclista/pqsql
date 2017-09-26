1c7c6577bbdd62dceac6b2d057d34abd7e8ea131
Thomas Krennwallner <krennwallner@ximes.com>
2017-09-26 15:33:37 +0200

    PqsqlTestsInitialize: make libpq.dll assembly resoltion work by using AssemblyInitialize to set %path% environment to libpqPath TestContext property

---
+++ PqsqlTests/PqsqlTestsInitialize.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PqsqlTests
+{
+	/// <summary>
+	/// setup PATH for libpq.dll assembly resolution
+	/// </summary>
+	[TestClass]
+	public class PqsqlTestsInitialize
+	{
+		[AssemblyInitialize]
+		public static void Initialize(TestContext context)
+		{
+			string libpqPath = context.Properties["libpqPath"].ToString();
+			string path = Environment.GetEnvironmentVariable("path");
+			Environment.SetEnvironmentVariable("path", libpqPath + ";" + path, EnvironmentVariableTarget.Process);
+		}
+	}
+}
