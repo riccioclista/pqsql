367b84a8d71b1441b700ba0140a24807e2e99416
Thomas Krennwallner <krennwallner@ximes.com>
2018-01-17 12:32:12 +0100

    datetime.h: merge changes from postgresql/REL9_6_STABLE
    merged the following commits:
    
    commit ee943004466418595363d567f18c053bae407792
    Author: Bruce Momjian <bruce@momjian.us>
    Date:   Sat Jan 2 13:33:40 2016 -0500
    
        Update copyright for 2016

--- libpqbinfmt/pgadt/datetime.h
+++ libpqbinfmt/pgadt/datetime.h
@@ -16,6 +16,23 @@
 #ifndef DATETIME_H
 #define DATETIME_H
 
+/* ----------------------------------------------------------------
+ *				time types + support macros
+ *
+ * String definitions for standard time quantities.
+ *
+ * These strings are the defaults used to form output time strings.
+ * Other alternative forms are hardcoded into token tables in datetime.c.
+ * ----------------------------------------------------------------
+ */
+
+/*
+ * Datetime input parsing routines (ParseDateTime, DecodeDateTime, etc)
+ * return zero or a positive value on success.  On failure, they return
+ * one of these negative code values.  DateTimeParseError may be used to
+ * produce a correct ereport.
+ */
+
 extern void j2date(int jd, int *year, int *month, int *day);
 extern int	date2j(int year, int month, int day);
 
