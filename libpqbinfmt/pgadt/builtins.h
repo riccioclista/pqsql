b51d114aa22a030bab0e7b96ca9fcac08391e9f9
Thomas Krennwallner <krennwallner@ximes.com>
2018-01-17 12:25:46 +0100

    builtins.h: merge changes from postgresql/REL9_6_STABLE
    merged the following commits:
    
    commit ed0097e4f9e6b1227935e01fa67f12a238b66064
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Date:   Sat Aug 13 18:31:14 2016 -0400
    
        Add SQL-accessible functions for inspecting index AM properties.

--- libpqbinfmt/pgadt/builtins.h
+++ libpqbinfmt/pgadt/builtins.h
@@ -23,6 +23,26 @@
  *		Defined in adt/
  */
 
+/* acl.c */
+
+/* amutils.c */
+
+/* bool.c */
+
+/* char.c */
+
+/* domains.c */
+
+/* encode.c */
+
+/* enum.c */
+
+/* int.c */
+
+/* name.c */
+
+/* numutils.c */
+
 /* float.c */
 
 extern double get_float8_infinity(void);
@@ -33,11 +53,113 @@ extern double float8in_internal(char *num, char **endptr_p,
 
 extern Datum float8in(char *num);
 
+/* dbsize.c */
+
+/* genfile.c */
+
+/* misc.c */
+
+/* oid.c */
+
+/* orderedsetaggs.c */
+
+/* pseudotypes.c */
+
+/* regexp.c */
+
+/* regproc.c */
+
+/* rowtypes.c */
+
+/* ruleutils.c */
+
+/* tid.c */
+
+/* varchar.c */
+
+/* varlena.c */
+
+/* version.c */
+
+/* xid.c */
+
+/* like.c */
+
+/* oracle_compat.c */
+
+/* inet_cidr_ntop.c */
+
+/* inet_net_pton.c */
+
+/* network.c */
+
+/* mac.c */
+
 /* numeric.c */
+
 extern Datum numeric_out(Numeric num);
 extern Datum numeric_recv(const char *buf, int32 typmod);
+
 extern Datum numeric_send(PQExpBuffer buf, Numeric num);
-extern Datum numeric_float8(Numeric num);
 extern Datum float8_numeric(double val);
+extern Datum numeric_float8(Numeric num);
+
+/* ri_triggers.c */
+
+/* trigfuncs.c */
+
+/* encoding support functions */
+
+/* format_type.c */
+
+/* quote.c */
+
+/* guc.c */
+
+/* pg_config.c */
+
+/* pg_controldata.c */
+
+/* rls.c */
+
+/* lockfuncs.c */
+
+/* txid.c */
+
+/* uuid.c */
+
+/* windowfuncs.c */
+
+/* access/spgist/spgquadtreeproc.c */
+
+/* access/spgist/spgkdtreeproc.c */
+
+/* access/spgist/spgtextproc.c */
+
+/* access/gin/ginarrayproc.c */
+
+/* access/tablesample/bernoulli.c */
+
+/* access/tablesample/system.c */
+
+/* access/transam/twophase.c */
+
+/* access/transam/multixact.c */
+
+/* access/transam/committs.c */
+
+/* catalogs/dependency.c */
+
+/* catalog/objectaddress.c */
+
+/* commands/constraint.c */
+
+/* commands/event_trigger.c */
+
+/* commands/extension.c */
+
+/* commands/prepare.c */
+
+/* utils/mmgr/portalmem.c */
 
 #endif   /* BUILTINS_H */
