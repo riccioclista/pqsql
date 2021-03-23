2403e90c0deeb70179887da9abf21509966a8818
Antonius Riha <antoniusriha@gmail.com>
2021-03-23 10:41:06 +0100

    Fix lib path resolution for tests on linux

--- PqsqlTests/PqsqlTestsInitialize.cs
+++ PqsqlTests/PqsqlTestsInitialize.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Runtime.InteropServices;
-using System.IO;
 
 namespace PqsqlTests
 {
@@ -15,14 +13,6 @@ namespace PqsqlTests
 		[AssemblyInitialize]
 		public static void Initialize(TestContext context)
 		{
-			// https://msdn.microsoft.com/en-us/library/ms682586.aspx
-			object libpqPath = context.Properties["libpqPath"];
-			if (libpqPath != null)
-			{
-				string path = Environment.GetEnvironmentVariable("PATH");
-				Environment.SetEnvironmentVariable("PATH", $"{libpqPath}{Path.PathSeparator}{path}", EnvironmentVariableTarget.Process);
-			}
-
 			// https://www.postgresql.org/docs/current/static/libpq-pgservice.html
 			object pgServiceFile = context.Properties["pgServiceFile"];
 			if (pgServiceFile != null)
